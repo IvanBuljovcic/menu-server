d0f1db06e9bcc0aaa20da1a008f9a9f7ef16290c
Ivan Buljovčić <ivan.buljovcic@gmail.com>
2024-11-25 10:10:56 +0100

    render config

--- src/app.service.ts
+++ src/app.service.ts
@@ -20,7 +20,6 @@ export class AppService {
     const browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath: '/usr/bin/chromium-browser',
     });
 
     const page = await browser.newPage();
